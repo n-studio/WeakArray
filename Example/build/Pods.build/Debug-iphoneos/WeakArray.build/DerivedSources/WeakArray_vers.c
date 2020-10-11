c891c83049b0225bbbecace29d216632b287f410
Matthew Nguyen <contact@n-studio.fr>
2020-10-11 19:47:41 +0200

    chore: update for XCode 12

--- Example/build/Pods.build/Debug-iphoneos/WeakArray.build/DerivedSources/WeakArray_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char WeakArrayVersionString[];
- extern const double WeakArrayVersionNumber;
-
- const unsigned char WeakArrayVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:WeakArray  PROJECT:Pods-1" "\n";
- const double WeakArrayVersionNumber __attribute__ ((used)) = (double)1.;
